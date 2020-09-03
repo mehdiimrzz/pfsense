f1d682dcb3149d586e6f299f8614e2bbfef4d58e
Mehdi Mirzaie <Mehdiimrz@gmail.com>
2020-09-03 15:24:41 +0430

    packages

---
+++ src/usr/local/custom_packages/install.sh
@@ -0,0 +1,2 @@
+pkg add pfSense-pkg-squidGuard-1.16.18_6.txz
+pkg add pfSense-pkg-squid-0.4.44_30.txz
--- src/usr/local/pkg/custom_packages/script.sh
+++
@@ -1,2 +0,0 @@
-pgk add pfSense-pkg-squidGuard-1.16.18_6.txz
-pgk add pfSense-pkg-squid-0.4.44_30.txz
