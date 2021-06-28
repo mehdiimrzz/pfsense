8f2781a8ff3356af825cf403af6c2dc27e936e0c
Mehdi Mirzaie <mehdi.mrz@live.com>
2021-06-28 18:09:10 +0430

    add snort rules

--- src/usr/local/custom_packages/install.sh
+++ src/usr/local/custom_packages/install.sh
@@ -1,3 +1,6 @@
 pkg add pfSense-pkg-squidGuard-1.16.18_6.txz
 pkg add pfSense-pkg-squid-0.4.44_30.txz
 pkg add pfSense-pkg-snort-4.1.2_3.txz
+cp -r snort_rules_up /tmp
+mv snort_check_for_updates.php /usr/local/pkg/snort
+/usr/local/bin/php-cgi -f /usr/local/pkg/snort/snort_check_for_updates.php
